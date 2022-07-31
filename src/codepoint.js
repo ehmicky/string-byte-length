c233f47e923c87360daf7db8b2cf49150ab85439
ehmicky <ehmicky@gmail.com>
2022-07-31 17:22:09 +0200

    Add comments

--- src/codepoint.js
+++ src/codepoint.js
@@ -9,13 +9,20 @@ export const getCodePointByteLength = function (string) {
   for (let charIndex = 0; charIndex < charLength; charIndex += 1) {
     const codepoint = string.charCodeAt(charIndex)
 
+    // ASCII characters -> 1 byte
     if (codepoint < 0x80) {
       hasSurrogate = false
+      // U+0080 to U+07ff -> 2 bytes
     } else if (codepoint < 0x8_00) {
       byteLength += 1
       hasSurrogate = false
+      // Astral character
     } else if (hasSurrogate && codepoint >= 0xdc_00 && codepoint <= 0xdf_ff) {
       hasSurrogate = false
+      // U+0800 to U+ffff -> 3 bytes
+      // However, U+d800 to U+dbff:
+      //  - Followed by U+dc00 to U+dfff -> 4 bytes together (astral character)
+      //  - Otherwise -> 3 bytes (like above)
     } else {
       byteLength += 2
       hasSurrogate = codepoint >= 0xd8_00 && codepoint <= 0xdb_ff

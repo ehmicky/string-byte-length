f08be39d3d423943a2ab118aef84e39bad6d6803
ehmicky <ehmicky@gmail.com>
2022-07-31 15:51:43 +0200

    Refactor tests

---
+++ test/helpers/main.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line ava/no-ignored-test-files
+import test from 'ava'
+import stringByteLength from 'string-byte-length'
+import { each } from 'test-each'
+
+import { ALL_STRINGS } from './strings.js'
+
+// Add a human-friendly test `title`
+const INPUTS = ALL_STRINGS.map(({ string, size }) => ({
+  string,
+  size,
+  // eslint-disable-next-line no-magic-numbers
+  title: JSON.stringify(string.toString(16)),
+}))
+
+each(INPUTS, ({ title }, { string, size }) => {
+  test(`Should compute the byte length | ${title}`, (t) => {
+    t.is(stringByteLength(string), size)
+  })
+})
---
+++ test/helpers/strings.js
@@ -0,0 +1,40 @@
+// All characters being tested
+const CHARACTERS = [
+  // ASCII
+  { string: '\0', size: 1 },
+  { string: '\u0001', size: 1 },
+  { string: '\b', size: 1 },
+  { string: '\t', size: 1 },
+  { string: '\n', size: 1 },
+  { string: 'a', size: 1 },
+  { string: ' ', size: 1 },
+  { string: '\u007F', size: 1 },
+  // Non-ASCII nor astral before U+0800
+  { string: '\u0080', size: 2 },
+  { string: '\u07FF', size: 2 },
+  // Non-ASCII nor astral since U+0800
+  { string: '\u0800', size: 3 },
+  { string: '\uFFFF', size: 3 },
+  // Astral characters
+  { string: '\uD800\uDC00', size: 4 },
+  { string: '\uDBFF\uDFFF', size: 4 },
+  { string: '\u{10000}', size: 4 },
+  { string: '\u{1FFFF}', size: 4 },
+  { string: '\u{FFFFF}', size: 4 },
+  // Invalid surrogate pairs
+  { string: '\uD800', size: 3 },
+  { string: '\uDBFF', size: 3 },
+  { string: '\uDC00', size: 3 },
+  { string: '\uDFFF', size: 3 },
+  { string: '\uDC00\uD800', size: 6 },
+]
+
+// Try each character with prepended|appended characters
+const STRINGS = CHARACTERS.flatMap(({ string, size }) => [
+  { string, size },
+  { string: `${string} `, size: size + 1 },
+  { string: ` ${string}`, size: size + 1 },
+])
+
+// Also test empty strings
+export const ALL_STRINGS = [{ string: '', size: 0 }, ...STRINGS]

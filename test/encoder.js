002c9d5a097ff4f726c8c9d53ea49852a07605aa
ehmicky <ehmicky@gmail.com>
2022-07-31 15:52:17 +0200

    Add more tests

--- test/encoder.js
+++ test/encoder.js
@@ -1,6 +1,6 @@
 // Test using `TextEncoder`, if supported
 
-// Similar browsers where `Buffer` is not available
+// Simulate browsers where `Buffer` is not available
 // eslint-disable-next-line fp/no-delete, n/prefer-global/buffer, import/unambiguous
 delete globalThis.Buffer.byteLength
 
---
+++ test/string.js
@@ -0,0 +1,9 @@
+// Test using `string.codePointAt()`
+
+// Simulate platforms where neither `Buffer` nor `TextEncoder` is available
+// eslint-disable-next-line fp/no-delete, n/prefer-global/buffer, import/unambiguous
+delete globalThis.Buffer.byteLength
+// eslint-disable-next-line fp/no-delete
+delete globalThis.TextEncoder
+
+await import('./helpers/main.js')

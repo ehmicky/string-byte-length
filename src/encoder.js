cd82bb81c55d7e2df8d1743cb5d6eea84e3ecc8c
ehmicky <ehmicky@gmail.com>
2022-07-31 15:52:28 +0200

    Add comments

--- src/encoder.js
+++ src/encoder.js
@@ -1,7 +1,9 @@
+// Initialize the function
 export const createTextEncoderFunc = function () {
   return getTextEncoderByteLength.bind(undefined, new TextEncoder())
 }
 
+// Compute the byte length using `TextEncoder`
 const getTextEncoderByteLength = function (textEncoder, string) {
   return textEncoder.encode(string).length
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createTextEncoderFunc, TEXT_ENCODER_MIN_LENGTH } from './encoder.js'
 import { getStringByteLength } from './string.js'
 
+// Retrieve the best method based on the platform support
 const getMainFunction = function () {
   // eslint-disable-next-line n/prefer-global/buffer
   if ('Buffer' in globalThis && 'byteLength' in globalThis.Buffer) {

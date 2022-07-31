fd6176b2530be9d1813de70411985cdc4871acf6
ehmicky <ehmicky@gmail.com>
2022-07-31 19:17:28 +0200

    Refactoring

--- src/char_code.js
+++ src/char_code.js
@@ -1,7 +1,7 @@
 // Uses imperative code for performance.
 // Uses `string.charCodeAt()` over `String.codePointAt()` because it is faster.
 /* eslint-disable complexity, max-statements, fp/no-let, fp/no-loops, max-depth,
-   fp/no-mutation, no-magic-numbers, unicorn/prefer-code-point */
+   fp/no-mutation, unicorn/prefer-code-point */
 export const getCharCodeByteLength = function (string) {
   const charLength = string.length
   let byteLength = charLength
@@ -10,27 +10,38 @@ export const getCharCodeByteLength = function (string) {
   for (let charIndex = 0; charIndex < charLength; charIndex += 1) {
     const codepoint = string.charCodeAt(charIndex)
 
-    // ASCII characters -> 1 byte
-    if (codepoint < 0x80) {
+    if (codepoint <= LAST_ASCII_CODEPOINT) {
       hasSurrogate = false
-      // U+0080 to U+07ff -> 2 bytes
-    } else if (codepoint < 0x8_00) {
+    } else if (codepoint <= LAST_TWO_BYTES_CODEPOINT) {
       byteLength += 1
       hasSurrogate = false
-      // Astral character
-    } else if (hasSurrogate && codepoint >= 0xdc_00 && codepoint < 0xe0_00) {
+    } else if (
+      hasSurrogate &&
+      codepoint >= FIRST_HIGH_SURROGATE &&
+      codepoint <= LAST_HIGH_SURROGATE
+    ) {
       hasSurrogate = false
-      // U+0800 to U+ffff -> 3 bytes
-      // However, U+d800 to U+dbff:
-      //  - Followed by U+dc00 to U+dfff -> 4 bytes together (astral character)
-      //  - Otherwise -> 3 bytes (like above)
     } else {
       byteLength += 2
-      hasSurrogate = codepoint >= 0xd8_00 && codepoint < 0xdc_00
+      hasSurrogate =
+        codepoint >= FIRST_LOW_SURROGATE && codepoint <= LAST_LOW_SURROGATE
     }
   }
 
   return byteLength
 }
+
+// Last ASCII character (1 byte)
+const LAST_ASCII_CODEPOINT = 0x7f
+// Last 2-bytes character
+const LAST_TWO_BYTES_CODEPOINT = 0x7_ff
+// Others are 3 bytes characters
+// However, U+d800 to U+dbff:
+//  - Followed by U+dc00 to U+dfff -> 4 bytes together (astral character)
+//  - Otherwise -> 3 bytes (like above)
+const FIRST_LOW_SURROGATE = 0xd8_00
+const LAST_LOW_SURROGATE = 0xdb_ff
+const FIRST_HIGH_SURROGATE = 0xdc_00
+const LAST_HIGH_SURROGATE = 0xdf_ff
 /* eslint-enable complexity, max-statements, fp/no-let, fp/no-loops, max-depth,
-   fp/no-mutation, no-magic-numbers, unicorn/prefer-code-point */
+   fp/no-mutation, unicorn/prefer-code-point */

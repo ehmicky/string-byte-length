be2c9ccc95f637e5dc3fe4f7aacb46c5136a82c0
ehmicky <ehmicky@gmail.com>
2022-07-31 17:18:22 +0200

    Improve performance

--- src/codepoint.js
+++ src/codepoint.js
@@ -3,20 +3,20 @@
    fp/no-mutation, no-magic-numbers */
 export const getCodePointByteLength = function (string) {
   const charLength = string.length
-  let byteLength = 0
+  let byteLength = charLength
 
   for (let charIndex = 0; charIndex < charLength; charIndex += 1) {
     const codepoint = string.codePointAt(charIndex)
 
-    if (codepoint < 0x80) {
-      byteLength += 1
-    } else if (codepoint < 0x8_00) {
-      byteLength += 2
-    } else if (codepoint < 0x1_00_00) {
-      byteLength += 3
-    } else {
-      byteLength += 4
-      charIndex += 1
+    if (codepoint >= 0x80) {
+      if (codepoint < 0x8_00) {
+        byteLength += 1
+      } else if (codepoint < 0x1_00_00) {
+        byteLength += 2
+      } else {
+        byteLength += 3
+        charIndex += 1
+      }
     }
   }
 

cfdde7a4c9e22e68f69635edf9d7e5be01a9ae61
ehmicky <ehmicky@gmail.com>
2022-08-07 23:54:41 +0200

    Add missing types documentation

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,7 +1,14 @@
 /**
+ * Get the UTF-8 byte length of a string.
  *
  * @example
  * ```js
+ * stringByteLength('test') // 4
+ * stringByteLength(' ') // 1
+ * stringByteLength('\0') // 1
+ * stringByteLength('±') // 2
+ * stringByteLength('★') // 3
+ * stringByteLength('🦄') // 4
  * ```
  */
 export default function stringByteLength<T extends string>(

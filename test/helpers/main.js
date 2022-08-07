e22ef96ddd438b27e6c3586754de5574676f88c3
ehmicky <ehmicky@gmail.com>
2022-08-07 15:42:24 +0200

    Simplify tests

--- test/helpers/main.js
+++ test/helpers/main.js
@@ -5,15 +5,7 @@ import { each } from 'test-each'
 
 import { ALL_STRINGS } from './strings.js'
 
-// Add a human-friendly test `title`
-const INPUTS = ALL_STRINGS.map(({ string, size }) => ({
-  string,
-  size,
-  // eslint-disable-next-line no-magic-numbers
-  title: JSON.stringify(string.replace(/_+/u, '_').toString(16)),
-}))
-
-each(INPUTS, ({ title }, { string, size }) => {
+each(ALL_STRINGS, ({ title }, { string, size }) => {
   test(`Should compute the byte length | ${title}`, (t) => {
     t.is(stringByteLength(string), size)
   })

e67ecec94c1a877df31e723d81d7b9682ef74525
ehmicky <ehmicky@gmail.com>
2022-08-07 15:34:36 +0200

    Fix typo

--- src/char_code.js
+++ src/char_code.js
@@ -20,7 +20,7 @@ export const getCharCodeByteLength = function (string) {
 
     byteLength += 2
 
-    if (codepoint < FIRST_LOW_SURROGATE || codepoint > LAST_LOW_SURROGATE) {
+    if (codepoint < FIRST_HIGH_SURROGATE || codepoint > LAST_HIGH_SURROGATE) {
       continue
     }
 
@@ -28,13 +28,13 @@ export const getCharCodeByteLength = function (string) {
     // next condition
     const nextCodepoint = string.charCodeAt(charIndex + 1)
 
-    // Low surrogates should be followed by high surrogates.
+    // High surrogates should be followed by low surrogates.
     // However, JavaScript strings allow invalid surrogates, which are counted
     // as a normal 3-byte character. This should not happen often in real code
     // though.
     if (
-      nextCodepoint < FIRST_HIGH_SURROGATE ||
-      nextCodepoint > LAST_HIGH_SURROGATE
+      nextCodepoint < FIRST_LOW_SURROGATE ||
+      nextCodepoint > LAST_LOW_SURROGATE
     ) {
       continue
     }
@@ -53,9 +53,9 @@ const LAST_TWO_BYTES_CODEPOINT = 0x7_ff
 // However, U+d800 to U+dbff:
 //  - Followed by U+dc00 to U+dfff -> 4 bytes together (astral character)
 //  - Otherwise -> 3 bytes (like above)
-const FIRST_LOW_SURROGATE = 0xd8_00
-const LAST_LOW_SURROGATE = 0xdb_ff
-const FIRST_HIGH_SURROGATE = 0xdc_00
-const LAST_HIGH_SURROGATE = 0xdf_ff
+const FIRST_HIGH_SURROGATE = 0xd8_00
+const LAST_HIGH_SURROGATE = 0xdb_ff
+const FIRST_LOW_SURROGATE = 0xdc_00
+const LAST_LOW_SURROGATE = 0xdf_ff
 /* eslint-enable complexity, max-statements, fp/no-let, fp/no-loops, max-depth,
    fp/no-mutation, no-continue, unicorn/prefer-code-point */

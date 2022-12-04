7519936f19133b1e07c36101d7fd8ed1603f5d1c
ehmicky <ehmicky@gmail.com>
2022-12-04 20:13:47 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,8 +1,9 @@
-import { expectType, expectNotType, expectError } from 'tsd'
+import { expectType, expectNotType } from 'tsd'
 
 import stringByteLength from 'string-byte-length'
 
 expectType<0>(stringByteLength(''))
 expectNotType<0>(stringByteLength('a'))
 expectType<number>(stringByteLength('a'))
-expectError(stringByteLength(true))
+// @ts-expect-error
+stringByteLength(true)

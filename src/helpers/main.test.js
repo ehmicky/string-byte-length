62dd6835b3d627b1e0156dee90aae1c06e573cc7
ehmicky <ehmicky@gmail.com>
2023-05-13 02:15:35 +0100

    Fix linting

--- src/helpers/main.test.js
+++ src/helpers/main.test.js
@@ -5,7 +5,6 @@ import { each } from 'test-each'
 
 import { ALL_STRINGS } from './strings.test.js'
 
-
 each(ALL_STRINGS, ({ title }, { string, size }) => {
   test(`Should compute the byte length | ${title}`, (t) => {
     t.is(stringByteLength(string), size)
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,7 +1,6 @@
 import stringByteLength from 'string-byte-length'
 import { expectType, expectNotType } from 'tsd'
 
-
 expectType<0>(stringByteLength(''))
 expectNotType<0>(stringByteLength('a'))
 expectType<number>(stringByteLength('a'))

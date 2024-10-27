85975838274640ad91432ebe3e583961087fdaba
ehmicky <ehmicky@gmail.com>
2024-10-27 23:35:04 +0000

    Fix tests in CI

--- src/helpers/main.test.js
+++ src/helpers/main.test.js
@@ -1,13 +1,15 @@
-// eslint-disable-next-line ava/no-ignored-test-files
 import test from 'ava'
+import isCI from 'is-ci'
 import { each } from 'test-each'
 
 import { ALL_STRINGS } from './strings.test.js'
 
 import stringByteLength from 'string-byte-length'
 
+const testFunction = isCI ? test.serial : test
+
 each(ALL_STRINGS, ({ title }, { string, size }) => {
-  test(`Should compute the byte length | ${title}`, (t) => {
+  testFunction(`Should compute the byte length | ${title}`, (t) => {
     t.is(stringByteLength(string), size)
   })
 })

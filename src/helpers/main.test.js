0e0c26ae17a71e727fa8cdafa716f96196711176
ehmicky <ehmicky@gmail.com>
2024-10-28 18:26:14 +0000

    Revert "Fix tests in CI"
    This reverts commit 85975838274640ad91432ebe3e583961087fdaba.

--- src/helpers/main.test.js
+++ src/helpers/main.test.js
@@ -1,15 +1,13 @@
+// eslint-disable-next-line ava/no-ignored-test-files
 import test from 'ava'
-import isCI from 'is-ci'
 import { each } from 'test-each'
 
 import { ALL_STRINGS } from './strings.test.js'
 
 import stringByteLength from 'string-byte-length'
 
-const testFunction = isCI ? test.serial : test
-
 each(ALL_STRINGS, ({ title }, { string, size }) => {
-  testFunction(`Should compute the byte length | ${title}`, (t) => {
+  test(`Should compute the byte length | ${title}`, (t) => {
     t.is(stringByteLength(string), size)
   })
 })
